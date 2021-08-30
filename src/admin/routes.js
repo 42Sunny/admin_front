fbdadcbfa761edbb3132e0ef31a5069fade9cbe7
LeeJuhae <tlatmsdlwngo@naver.com>
2021-08-30 17:31:05 +0900

    ADMN-1 CHECKIN admin 코드 추가
    CHECKIN 프로젝트에서 개발한 admin 코드 추가했습니다.
    현재 CHECKIN admin 코드는 에러 없이 작동되는 상태입니다.
    
    원래 CHECKIN, VISITOR admin이 각각의 프로젝트에서 개발되고 있었기 때문에
    admin template 코드 일부가 중복되어 있습니다.
    추후 각각의 코드를 모두 안정화시킨 다음 중복 코드 제거하겠습니다.

--- src/admin/routes.js
+++ src/admin/routes.js
@@ -2,6 +2,7 @@ import Dashboard from '@material-ui/icons/Dashboard';
 import Person from '@material-ui/icons/Person';
 import DashboardPage from 'admin/views/Dashboard/Dashboard.js';
 import UserProfile from 'admin/views/UserProfile/UserProfile.js';
+import AdminPage from '../checkin-admin/views/AdminPage';
 
 const dashboardRoutes = [
   {
@@ -16,6 +17,13 @@ const dashboardRoutes = [
     name: '출입 관리',
     icon: Person,
     component: UserProfile,
+    layout: '/admin',
+  },
+  {
+    path: '/checkin',
+    name: '체크인 관리',
+    icon: Person,
+    component: AdminPage,
     layout: '/admin',
   },
 ];
