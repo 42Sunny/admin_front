4dc0989848240410023b73c6616a1177207966de
idjaeha <jaehanim1@gmail.com>
2021-08-30 15:42:16 +0900

    visitor 어드민 페이지 이전

---
+++ src/admin/index.js
@@ -0,0 +1,32 @@
+import React, { useContext } from 'react';
+// import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+
+import './assets/css/material-dashboard-react.css?v=1.10.0';
+import Login from './components/Login/Login';
+import { LoginContext } from './contexts/LoginContext';
+import Admin from './layouts/Admin';
+
+const AdminPage = () => {
+  const { isLogin } = useContext(LoginContext);
+
+  return (
+    <BrowserRouter>
+      <Switch>
+        {isLogin === true ? (
+          <>
+            <Route path="/admin" component={Admin} />
+            <Redirect from="/" to="/admin/dashboard" />
+          </>
+        ) : (
+          <>
+            <Route path="/admin" component={Login} />
+            <Redirect from="*" to="/admin" />
+          </>
+        )}
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default AdminPage;
