b1916c321d4f2c0aa9535cc3ab3628915dec2613
6mn12j <dbwk0404@hanmail.net>
2022-03-13 19:21:59 +0900

    Revert "운영시간 변경 기능 추가"
    This reverts commit 93b5c87ac44a7d6f1203e8ced2f533cb9a24e742.

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -2,9 +2,3 @@ import dayjs from 'dayjs';
 
 export const formatDate = (format: string = 'YYYY-MM-DD', date?: Parameters<typeof dayjs>[0]) =>
   dayjs(date).format(format);
-
-export const stringToDate = (date: any) => {
-  const startHour: number = date.split(':')[0] * 1;
-  const startMinute: number = date.split(':')[1] * 1;
-  return dayjs().hour(startHour).minute(startMinute);
-};
--- src/views/Settings/Settings.jsx
+++ src/views/Settings/Settings.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import StaffTable from 'components/Staff/StaffTable';
 import MaxHeadCount from 'components/MaxHeadCount/MaxHeadCount';
-import TimeSetting from 'components/TimeSetting';
 
 const Settings = () => {
   return (
     <>
-      <TimeSetting />
       <MaxHeadCount />
       <StaffTable />
     </>
