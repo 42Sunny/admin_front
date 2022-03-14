e94624280264ba819d656148d5b7a19a663e4b57
idjaeha <jaehanim1@gmail.com>
2022-03-14 11:36:58 +0000

    TimeSetting 컴포넌트를 다시 생성

--- src/views/Settings/Settings.jsx
+++ src/views/Settings/Settings.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import StaffTable from 'components/Staff/StaffTable';
 import MaxHeadCount from 'components/MaxHeadCount/MaxHeadCount';
+import TimeSetting from 'components/TimeSetting';
 
 const Settings = () => {
   return (
     <>
+      <TimeSetting />
       <MaxHeadCount />
       <StaffTable />
     </>
