9d779be26ba6a703d673beb888ca49a4ac5dd2c2
idjaeha <jaehanim1@gmail.com>
2022-04-14 03:03:17 +0900

    총원이 나타나지 않는 오류 수정

--- src/utils/getHeadCount.js
+++ src/utils/getHeadCount.js
@@ -2,9 +2,9 @@ import { getConfig } from 'API/checkin/config';
 
 export const getHeadCount = async (setMaxGaepo, setMaxSeocho) => {
   try {
-    const { data: payload } = await getConfig();
-    setMaxGaepo(payload.gaepo);
-    setMaxSeocho(payload.seocho);
+    const response = await getConfig();
+    setMaxGaepo(response.data.payload.gaepo);
+    setMaxSeocho(response.data.payload.seocho);
   } catch (err) {
     console.log(err);
   }
